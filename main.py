822c3439949b037e0367005fcfc5e19141c70baf
Andrey Dyatlov <andrey@mrbot.im>
2017-11-24 19:38:22 +0300

    first

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ data_Branches = pandas.read_excel('case1/Materials for local case/Customer codes
 data_Customers = pandas.read_excel('case1/Materials for local case/Customer codes.xlsx', sheetname='Customers')
 print(data_Branches)
 print(data_Customers)
+
+data = pandas.read_csv('case1/Materials for local case/pg-recommender/data/data.csv')
+print(data)
+
+op_src_parameters.csv
